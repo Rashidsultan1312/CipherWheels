48f6f0048d7c26758228103fd2a4abb243894af0
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-24 13:05:19 +0300

    Update AppConfig.swift

--- CipherWheels/Disc/AppConfig.swift
+++ CipherWheels/Disc/AppConfig.swift
@@ -2,8 +2,8 @@ import Foundation
 
 enum AppConfig {
     static let cipherLink = URL(string: "https://keitaro-zaglushka.com")!
-    static let privacyPolicyURL = URL(string: "https://hallowtommy.github.io/cipher-wheels-privacy")!
-    static let supportEmail = "support@hallowtommy.app"
+    static let privacyPolicyURL = URL(string: "https://www.termsfeed.com/live/f2c1a4ad-db8d-4dbc-b451-7db4b48e3643")!
+    static let supportEmail = "imra9uma@icloud.com"
 
     static var versionLine: String {
         let mv = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
